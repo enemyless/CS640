50b64603ee73adf0b82bf78ae7021c74f6c4a6aa
enemyless <enemyless@gmail.com>
2016-11-06 16:45:28 -0600

    task1

--- switchyard-master/examples/exercises/router/myrouter.py
+++ switchyard-master/examples/exercises/router/myrouter.py
@@ -11,9 +11,19 @@ from switchyard.lib.packet import *
 from switchyard.lib.address import *
 from switchyard.lib.common import *
 
+class mapingTableElement(object):
+    def __init__(self,ip=None,mac=None,TTL=64):
+        self.ip = ip
+        self.mac = mac
+        self.TTL = TTL
+
+    def display(self):
+        print ("ip=%s,mac=%s,TTL=%s\n" % (self.ip,self.mac,self.TTL))
+
 class Router(object):
     def __init__(self, net):
         self.net = net
+        self.mappingTable = []
         # other initialization stuff here
 
 
@@ -22,6 +32,8 @@ class Router(object):
         Main method for router; we stay in a loop in this method, receiving
         packets until the end of time.
         '''
+        my_interfaces = self.net.interfaces()
+
         while True:
             gotpkt = True
             try:
@@ -36,8 +48,25 @@ class Router(object):
             if gotpkt:
                 log_debug("Got a packet: {}".format(str(pkt)))
 
+                # ARP packet
+                arp_header = pkt.header(Arp)
+                    if arp_header:
 
+                    # ARP request
+                    if arp_header.targethwaddr == "":
+                        for item in mappingTable:
+                            if item.ip == arp_header.senderprotoaddr:
+                                item.mac = arp_header.senderhwaddr
+                                break
 
+                        for item in my_interfaces:
+                            if item.ipaddr == arp_header.targetprotoaddr:
+                                arp_reply == create_ip_art_reply(arp_header.senderhwaddr,arp_header.targethwaddr,arp_header.senderprotoaddr,arp_header.targetprotoaddr)
+                                self.net.send_packet(dev,arp_reply)
+                            break
+
+                        
+                        
 def switchy_main(net):
     '''
     Main entry point for router.  Just create Router
